c03c6d2f26dbfbe01ad7587e042633949af3bfa7
Tessa Payne <tessapayne@Tessas-MacBook-Pro.local>
2026-03-13 20:58:37 -0500

    Complete Hogwarts Shopping Cart Vue Components

---
+++ src/components/CartTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ username }}'s Shopping Cart</h1>
+</template>
+
+<script setup>
+defineProps(['username'])
+</script>
+
+<style scoped>
+h1 {
+  padding: 20px;
+  max-width: 1200px;
+  margin: auto;
+}
+</style>
